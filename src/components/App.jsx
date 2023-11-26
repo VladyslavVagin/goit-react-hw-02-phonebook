4f3734450739f0a91843fda3e334b2895f3677da
Vladyslav Vagin <140642012+VladyslavVagin@users.noreply.github.com>
2023-11-26 14:03:55 +0100

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,12 +6,7 @@ import Filter from './Filter/Filter';
 
 export class App extends Component {
   state = {
-    contacts: [
-      {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
-      {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
-      {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
-      {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
-    ],
+    contacts: [],
     filter: '',
   };
 
@@ -43,11 +38,10 @@ export class App extends Component {
     );
   };
 
-  deleteContact = (e) => {
-    console.log(e.target.id);
+  deleteContact = ({target}) => {
     this.setState(prevState => {
       return {
-        contacts: prevState.contacts.filter(contact => contact.id !== e.target.id)
+        contacts: prevState.contacts.filter(contact => contact.id !== target.id)
       }
     })
   }
