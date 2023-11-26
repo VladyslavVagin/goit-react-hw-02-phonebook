6aae6dbf713dedf49b1d6c7303e19b4a922bf911
Vladyslav Vagin <140642012+VladyslavVagin@users.noreply.github.com>
2023-11-26 13:14:12 +0100

    update

--- src/components/Contact/ContactUser.jsx
+++ src/components/Contact/ContactUser.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
-import css from './ContactUser.module.css'
+import css from './ContactUser.module.css';
 
-const ContactUser = (contact) => {
+const ContactUser = contact => {
   const { id, name, number } = contact;
   return (
     <li key={id} className={css.user}>
       <h3>{name}:</h3>
       <p>{number}</p>
-      <button type='button' className={css.deleteBtn}>Delete</button>
+      <button type="button" className={css.deleteBtn}>
+        Delete
+      </button>
     </li>
   );
 };
