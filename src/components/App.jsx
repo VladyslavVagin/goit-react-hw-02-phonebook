328c2c6a0f3141b7fdc9410fbde1d995cd23db27
Vladyslav Vagin <140642012+VladyslavVagin@users.noreply.github.com>
2023-11-26 12:28:47 +0100

    update
    step5

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -11,34 +11,42 @@ export class App extends Component {
   };
 
   createUser = data => {
-    const newUser = {
-      ...data,
-      id: nanoid(),
-    };
+    let namesOfUsers = [];
+    namesOfUsers = this.state.contacts.map(userName => userName.name);
+    if (!namesOfUsers.includes(data.name)) {
+      const newUser = {
+        ...data,
+        id: nanoid(),
+      };
 
-    // For change array in state , we need to create copy of array, change him, and then setState
-    const copyArray = this.state.contacts.slice(0);
-    copyArray.push(newUser);
-    this.setState({ contacts: copyArray });
+      // For change array in state , we need to create copy of array, change him, and then setState
+      const copyArray = this.state.contacts.slice(0);
+      copyArray.push(newUser);
+      return this.setState({ contacts: copyArray });
+    } else {
+      alert(`User ${data.name} already exist`);
+    }
   };
 
   changeFilter = e => this.setState({ filter: e.target.value });
 
   filterContacts = () => {
-    const {contacts, filter} = this.state;
+    const { contacts, filter } = this.state;
     const normalizedFilter = filter.toLocaleLowerCase();
-    return contacts.filter(contact => contact.name.toLocaleLowerCase().includes(normalizedFilter))
-  }
+    return contacts.filter(contact =>
+      contact.name.toLocaleLowerCase().includes(normalizedFilter)
+    );
+  };
 
   render() {
-    const {filter} = this.state;
+    const { filter } = this.state;
     const visibleContacts = this.filterContacts();
     return (
       <div className="container">
         <h1>Phonebook</h1>
         <AddContactForm createUser={this.createUser} />
         <h2>Contacts</h2>
-        <Filter value={filter} onChange={this.changeFilter}/>
+        <Filter value={filter} onChange={this.changeFilter} />
         {ListOfContacts(visibleContacts)}
       </div>
     );
